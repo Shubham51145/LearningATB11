954605f2db3884ab646072b8e15e0ca273ba8a43
nehakaremore <nehakaremore12@gmail.com>
2025-03-16 21:33:26 +0530

    9 table

--- src/Ex_01_Javabasic/Helloworld.java
+++ src/Ex_01_Javabasic/Helloworld.java
@@ -3,6 +3,7 @@ package Ex_01_Javabasic;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
     }
 
 }
---
+++ src/Ex_01_Javabasic/Lab01_printf.java
@@ -0,0 +1,23 @@
+package Ex_01_Javabasic;
+
+import java.sql.SQLOutput;
+
+public class Lab01_printf {
+    public static void main(String[] args) {
+
+        int table = 9;
+        System.out.printf("9 x 1 = %d\n", 9*1);
+        System.out.printf("9 * 2  %d\n" ,9*2);
+        System.out.printf("9 * 3  %d\n" ,9*3);
+        System.out.printf("9 * 4  %d\n" ,9*4);
+        System.out.printf("9 * 5  %d\n" ,9*5);
+        System.out.printf("9 * 6  %d\n" ,9*6);
+        System.out.printf("9 * 7  %d\n" ,9*7);
+        System.out.printf("9 * 8  %d\n" ,9*8);
+        System.out.printf("9 * 9  %d\n" ,9*9);
+        System.out.printf("9 * 10  %d\n" ,9*10);
+
+
+
+    }
+}
